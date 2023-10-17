723ad7e0245bf99a72ff44c974007784a4935238
veronica97williams <97235804+veronica97williams@users.noreply.github.com>
2023-10-16 21:47:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import * as THREE from 'three';
+import { GLTFLoader } from 'three';
 // Initialize Three.js scene
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
